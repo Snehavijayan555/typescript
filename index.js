c6841a76cd1bb124961b3944f315816350e66c50
Sneha Vijayan <125389679+Snehavijayan555@users.noreply.github.com>
2024-09-13 10:15:14 +0530

    basic concepts

---
+++ index.js
@@ -0,0 +1,16 @@
+// infer types(implicit types)
+// let userName="Sneha";
+// let subscribers=30_00;
+// Defining types(Explicit types)
+var userName = "Sneha";
+var subscribers = 30000;
+var isSubscribed = true;
+var skills = ["JS", "CSS", "TS"];
+var count = [1, 2, 3, 4];
+var emptyArray = [];
+var userDetail = {
+    name: "sneha",
+    age: 26,
+    salary: 30000,
+};
+userDetail.age = 20;
---
+++ index.ts
@@ -0,0 +1,110 @@
+// infer types(implicit types)
+
+// let userName="Sneha";
+// let subscribers=30_00;
+
+
+
+
+// Defining types(Explicit types)
+// let userName: string = "Sneha";
+// let subscribers: number = 30_000;
+// let isSubscribed: boolean =true;
+// let skills: string[] = ["JS","CSS","TS"];
+// let count : number[] = [1,2,3,4];
+// let emptyArray: [] = [];
+// let userDetail: {name: string; age:number; salary:number } = {
+//     name :"sneha",
+//     age : 26,
+//     salary:30_000,
+// };
+// userDetail.age=20;
+
+
+
+
+// interface
+
+// interface Details {
+// name:string;
+// age:number;
+// salary:number;
+// getName: () => void;
+// } 
+
+// let userDetail: Details = {
+//     name :"sneha",
+//     age : 26,
+//     salary:30_000,
+//     getName() {
+//         console.log(this.name);
+//     },
+// };
+
+
+
+
+// Type
+
+// type Details = {
+//     name:string;
+// age:number;
+// salary:number;
+// getName: () => void;
+// }
+
+// let userDetail: Details = {
+//     name :"sneha",
+//     age : 26,
+//     salary:30_000,
+//     getName() {
+//         console.log(this.name);
+//     },
+// };
+
+
+
+
+
+// Union/Optional
+
+// type Details = {
+//     name:string;
+// age:number | string;
+// salary:number;
+// getName: () => void;
+// }
+
+// let userDetail: Details = {
+//     name :"sneha",
+//     age : 26,
+//     salary:30_000,
+//     getName() {
+//         console.log(this.name);
+//     },
+// };
+
+// let skills: (string | number | boolean)[] = ["JS","CSS","TS",20,10,30,true];
+
+// optional
+
+// type Details = {
+//     name:string;
+// age:number | string;
+// salary?:number;
+// getName?: () => void;
+// }
+
+// let userDetail: Details = {
+//     name :"sneha",
+//     age : 26,
+//     salary:100,
+// };
+
+
+
+
+
+// Functions
+
+
