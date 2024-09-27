e2eb198352985f7e20724832f8b96e7b455bf733
Sneha Vijayan <125389679+Snehavijayan555@users.noreply.github.com>
2024-09-27 10:01:28 +0530

    function overloading

--- index.ts
+++ index.ts
@@ -135,9 +135,125 @@
 
 
 // Named Types
-type StatusType = "pending1" | "completed1" | "failed1" | "";
-type ToggleSwitch = "on" | "off";
+// type StatusType = "pending1" | "completed1" | "failed1" | "";
+// type ToggleSwitch = "on" | "off";
 
-let currentStatus : StatusType = "";
-let toggleSwitch : ToggleSwitch = "off";
+// let currentStatus : StatusType = "";
+// let toggleSwitch : ToggleSwitch = "off";
 
+
+
+
+
+// Function Overloading
+
+// with numbers
+
+// function add(num1:number,num2:number):number{
+//     return num1+num2;
+// }
+// add(2,2);
+
+
+// one function with different datatype
+
+// function add(num1:number | string,num2:number | string):number | string{
+//     if(typeof num1 === "string" || typeof num2 === "string"){
+//         return num1+ "" +num2;
+//     }
+//     return num1+num2;
+// }
+// add(2,2);
+// add("2","2");
+
+
+// properway of overloading
+
+// function add(num1:number ,num2:number ):number;
+// function add(num1:string ,num2:string ):string;
+
+// function add(num1:any ,num2:any ):any{
+//     return num1+num2;
+// }
+// add(2,2);
+// add("2","2");
+
+
+
+
+
+
+// Generics
+
+// example 1
+// function getAge<T>(age : T) : T{
+//     return age;
+// }
+// getAge<string>("20");
+// getAge<number>(20);
+
+
+// example 2
+// type UserDetail = {
+//     name : string;
+//     age : number;
+// }
+// type AdminDetail = {
+//     firstName : string;
+//     role : string;
+// }
+// const userDetails : UserDetail = {
+//     name : "Sneha",
+//     age : 26,
+// }
+// const adminDetails : AdminDetail = {
+//     firstName : "Sneha",
+//     role : "admin",
+// }
+
+//  function getDetails<T>(details : T) : T{
+//     return details;
+//  }
+// const userValue = getDetails<UserDetail>(userDetails);
+// const adminValue = getDetails<AdminDetail>(adminDetails);
+
+// userValue.name;
+// adminValue.role;
+
+
+// example 3 - using Type
+// type UserDetail = {
+//     name : string;
+//     age : number;
+// }
+// type AdminDetail = UserDetail & {
+//     role : string;
+// }
+// const userDetails : UserDetail = {
+//     name : "Sneha",
+//     age : 26,
+// }
+// const adminDetails : AdminDetail = {
+//     name : "Sneha",
+//     age : 20 ,
+//     role : "admin",
+// }
+
+
+// Using interface
+// interface UserDetail  {
+//     name : string;
+//     age : number;
+// }
+// interface AdminDetail extends UserDetail  {
+//     role : string;
+// }
+// const userDetails : UserDetail = {
+//     name : "Sneha",
+//     age : 26,
+// }
+// const adminDetails : AdminDetail = {
+//     name : "Sneha",
+//     age : 20 ,
+//     role : "admin",
+// }
